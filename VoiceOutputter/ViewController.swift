0b74608f4a74271d3931b5b654b7206ac3840008
syunnichi <s1411376@coins.tsukuba.ac.jp>
2015-01-30 13:14:17 +0900

    initial commit

---
+++ VoiceOutputter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceOutputter
+//
+//  Created by takayuki abe on 2015/01/30.
+//  Copyright (c) 2015年 University of Tsukuba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
